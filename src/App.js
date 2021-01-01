1fb7035f7f7f36d10e9d265b0d26901ab36d9593
Saurabh <saurabhj8087445@gmail.com>
2021-01-01 18:56:17 +0530

    Login and other files

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from "./Feed"
+import { selectUser } from './features/userSlice';
 
 function App() {
+  const user = userSelector(selectUser);
+
+
   return (
     <div className="app">
      <Header/>
+      {!user ? (
+        <Login/>
+      ): (
+        <div className="app__body">
+          <Sidebar/>
+          <Feed/>
+          
+        </div>
+      )
+      }
+
      <div className="app__body">
        <Sidebar/>
        <Feed/>
--- src/Feed.js
+++ src/Feed.js
@@ -1,57 +1,95 @@
-import React, { useState } from 'react'
-import "./Feed.css"
-import InputOption from "./InputOption"
+import React, { useEffect, useState } from "react";
+import "./Feed.css";
+import InputOption from "./InputOption";
 import CreateIcon from "@material-ui/icons/Create";
 import ImageIcon from "@material-ui/icons/Image";
 import YouTubeIcon from "@material-ui/icons/YouTube";
 import DescriptionIcon from "@material-ui/icons/Description";
 import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
 import Post from "./Post";
+import { db } from "./firebase";
+import firebase from "firebase";
 
 function Feed() {
-   const [posts,setPosts] = useState([]);
+  const [input, setInput] = useState("");
+  const [posts, setPosts] = useState([]);
 
-   const sendPost = e => {
-     e.preventDefault();
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+  }, []);
 
-   }
+  const sendPost = (e) => {
+    e.preventDefault();
+    db.collection("posts").add({
+      name: "Saurabh J",
+      description: "This is test",
+      message: input,
+      photoUrl: "",
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
 
-    return (
-      <div className="feed">
-        <div className="feed__inputContainer">
-          <div className="feed__input">
-            <CreateIcon />
-            <form action="">
-              <input type="text" name="" id="" />
-              <button onClick={sendPost} type="submit">Send</button>
-            </form>
-          </div>
-          <div className="feed__inputOptions">
-            <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
-            <InputOption Icon={YouTubeIcon} title="Video" color="#7fc15e" />
-            <InputOption
-              Icon={DescriptionIcon}
-              title="Document"
-              color="#c79ef7"
+    setInput("");
+  };
+
+  return (
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form action="">
+            <input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              type="text"
+              name=""
+              id=""
             />
-            <InputOption
-              Icon={CalendarViewDayIcon}
-              title="Write Article"
-              color="#f5987e"
-            />
-          </div>
+            <button onClick={sendPost} type="submit">
+              Send
+            </button>
+          </form>
         </div>
-
-        {posts.map((post)=>(
-           <Post/>
-        ))}
-        <Post 
-          name="Saurabh J" 
-          description="This is a test"
-          message="WOW this worked"
-        />
+        <div className="feed__inputOptions">
+          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOption Icon={YouTubeIcon} title="Video" color="#7fc15e" />
+          <InputOption
+            Icon={DescriptionIcon}
+            title="Document"
+            color="#c79ef7"
+          />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write Article"
+            color="#f5987e"
+          />
+        </div>
       </div>
-    );
+
+      {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+        <Post
+          key={id}
+          name={name}
+          description={description}
+          message={message}
+          photoUrl={photoUrl}
+        />
+      ))}
+      <Post
+        name="Saurabh J"
+        description="This is a test"
+        message="WOW this worked"
+      />
+    </div>
+  );
 }
 
-export default Feed
+export default Feed;
